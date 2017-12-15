93e7272e00b405300c1836c99e65d37c3f10de22
prajcharde <prajcharde@gmail.com>
2017-12-15 18:13:01 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from "angular2-social-login/dist";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnDestroy {
+  public user;
+  sub: any;
+  constructor(public _auth: AuthService) { }
+
+  signIn(provider) {
+    this.sub = this._auth.login(provider).subscribe(
+      (data) => {
+        console.log(data); this.user = data;
+      }
+    )
+  }
+
+  logout() {
+    this._auth.logout().subscribe(
+      (data) => { console.log(data); this.user = null; }
+    )
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Angular2SocialLoginModule } from "angular2-social-login";
 import { AppComponent } from './app.component';
 
+
+let providers = {
+  "google": {
+    "clientId": "346502243474-sa0bq8q5bu31hge781f29ratb14bcbis"
+  },
+  "facebook": {
+    "clientId": "357644274649684",
+    "apiVersion": "v2.4"
+  }
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Angular2SocialLoginModule
   ],
-  providers: [],
+  
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(){}
+ }
+Angular2SocialLoginModule.loadProvidersScripts(providers);
